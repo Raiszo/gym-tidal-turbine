fefdac3ea890308c12e3d732781e0e54a25be4d5
Raiszo <E05.sek@gmail.com>
2019-02-01 17:19:30 -0500

    :dagger: adding Cp function

--- gym-wind-turbine/envs/wind_turbine.py
+++ gym-wind-turbine/envs/wind_turbine.py
@@ -3,43 +3,42 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 
-
 class WindTurbine(gym.Env):
     T = 0.05
+
     
-    metadata = {
-        'render.modes': ['human'],
-        'video.frames_per_second' : 1/T
-    }
-
     def __init__(self):
-        self.gravity = 9.81 #: [m/s2] acceleration
-        self.mass = 0.5 #: [kg] mass
-        self.Ixx = 0.00232
-        self.arm_length = 0.1 # [m]
-        self.arm_width = 0.02 # [m]
-        self.height = 0.02 # [m]
+        """
+        The data is from a 5MW wind turbine for offshore systema development
+        """
+        self.beta = 0.0
+        self.alpha_0 = None
 
-        # max and min force for each motor
-        self.maxF = 2 * self.mass * self.gravity
-        self.minF = 0
-        self.maxAngle = np.pi / 2
-        self.dt = Drone.T
+        self.area_blade = 12445.3
+        self.radius = 63
+        self.k_constants = [
+            None,
+            0.73,
+            151.0,
+            0.58,
+            2e-3,
+            2.14,
+            13.2,
+            18.4
+        ]
+
+        self.vel_w = None
+        self.w_t = None
+        
+
+        self.dt = WintTurbine.T
 
         high = np.array([
-            1.0,
-            1.0,
-            np.finfo(np.float32).max,
-            np.finfo(np.float32).max,
-            # 1.0,
-            np.finfo(np.float32).max,
-            np.finfo(np.float32).max,
-            np.finfo(np.float32).max,
         ])
         
         self.action_space = spaces.Box(
-            low = np.array([self.minF, self.minF]),
-            high = np.array([self.maxF, self.maxF]),
+            low = np.array([]),
+            high = np.array([]),
             dtype = np.float32
         )
         self.observation_space = spaces.Box(
@@ -52,6 +51,30 @@ class WindTurbine(gym.Env):
         self.viewer = None
         self.state = None
 
+    @property
+    def Cp(self):
+        k = self.k_constants
+        beta = self.beta
+        
+        lamb_i = 1/(self.lamb + 8e-2*beta) - 35e-3/(1 + beta**3)
+        lamb_i = 1 / lamb_i
+
+        cp = k[1] * (k[2]/lamb_i  - k[3]*beta - k[4]*beta^k[5]- k[6]) * np.exp(-k[7]/lamb_i)
+
+        return cp
+
+    def _get_obs(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def _apply_action(self, u):
+        pass
+
+    def reset(self):
+        pass
+    
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
