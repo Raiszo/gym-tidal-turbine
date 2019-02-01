d8808074fa79d37ee482280154b3eef9bf4472c8
Raiszo <E05.sek@gmail.com>
2019-02-01 18:55:20 -0500

    :dagger: almost done ;'v

--- gym-wind-turbine/envs/wind_turbine.py
+++ gym-wind-turbine/envs/wind_turbine.py
@@ -11,11 +11,14 @@ class WindTurbine(gym.Env):
         """
         The data is from a 5MW wind turbine for offshore systema development
         """
+        self.rho = 0.0
         self.beta = 0.0
         self.alpha_0 = None
 
         self.area_blade = 12445.3
         self.radius = 63
+        self.friction = 0.0
+        self.J = 0.00
         self.k_constants = [
             None,
             0.73,
@@ -29,16 +32,22 @@ class WindTurbine(gym.Env):
 
         self.vel_w = None
         self.w_t = None
+        self.w_m = 0.0
+
+        self.minU = - np.finfo(np.float32).max
+        self.maxU = np.finfo(np.float32).max
         
 
         self.dt = WintTurbine.T
 
         high = np.array([
+            np.finfo(np.float32).max, # w_m
+            np.finfo(np.float32).max, # w_m_dot
         ])
         
         self.action_space = spaces.Box(
-            low = np.array([]),
-            high = np.array([]),
+            low = np.array([ self.minU, self.minU ]),
+            high = np.array([ self.maxU, self.maxU ]),
             dtype = np.float32
         )
         self.observation_space = spaces.Box(
@@ -46,6 +55,10 @@ class WindTurbine(gym.Env):
             high,
             dtype=np.float32
         )
+
+        self.__state = None
+
+        self.reset()
 
         self.seed()
         self.viewer = None
@@ -55,13 +68,26 @@ class WindTurbine(gym.Env):
     def Cp(self):
         k = self.k_constants
         beta = self.beta
-        
-        lamb_i = 1/(self.lamb + 8e-2*beta) - 35e-3/(1 + beta**3)
+
+        lamb = self.radius * self.w_m
+        lamb_i = 1/(lamb + 8e-2*beta) - 35e-3/(1 + beta**3)
         lamb_i = 1 / lamb_i
 
         cp = k[1] * (k[2]/lamb_i  - k[3]*beta - k[4]*beta^k[5]- k[6]) * np.exp(-k[7]/lamb_i)
 
         return cp
+
+    @property
+    def P_m(self):
+        return 0.5 * self.Cp * self.rho * self.area_blade * self.v_w**3
+
+    @property
+    def T_m(self):
+        return self.P_m / self.w_m
+
+    @property
+    def w_m(self):
+        return self.__state[0]
 
     def _get_obs(self):
         pass
@@ -70,10 +96,19 @@ class WindTurbine(gym.Env):
         pass
 
     def _apply_action(self, u):
-        pass
+        sdot = np.array([
+            w_m_dot,
+            (u - self.T_m - self.friction*self.w_m) / self.J
+        ])
+
+        self.state = sdot * self.dt + self.state
+
+        return 0
 
     def reset(self):
-        pass
+        self.__state = np.zeros(2)
+        self.w_m = 0
+        self.vel_w = 0
     
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
