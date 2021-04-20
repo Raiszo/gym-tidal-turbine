6181122304c8fd3903eafd404e92e7e0e86dc062
Raiszo <E05.sek@gmail.com>
2021-04-20 02:57:30 -0500

    fixing dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     install_requires=[
         'gym',
         'numpy',
+        'rl_agents @ git+https://github.com/Raiszo/rl-agents.git@master#egg=rl_agents',
         # 'CCBlade==1.1.1',
-        'matplotlib==2.0.0'     # keep this version for now
+        # 'matplotlib==2.0.0'     # keep this version for now
     ]
 )
